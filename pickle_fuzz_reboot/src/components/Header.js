9c25fa4522b5962deef3e194e6609288783feda8
Mattie-Marie <gonzalem@lssc.edu>
2023-05-10 18:13:18 -0400

    adding header

--- pickle_fuzz_reboot/src/components/Header.js
+++ pickle_fuzz_reboot/src/components/Header.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { Image } from '@chakra-ui/react'
+import header_image from './images/header_image.jpg';
+
 function Header () {
     return (
-        <div>
-
+      <div className='header-background'>
+        <div className='header-text'>
+          Welcome to the Fuzz
         </div>
+        <div className='header-container'>
+          <Image
+            className='header-image'
+            src={header_image}
+            alt='Header Image'
+            borderRadius='lg'
+          />
+        </div>
+      </div>
       );
     }
     
